ac4bde8f285ebc1494963d769b2283cb52b2d194
Rohit8451 <rohitgchavan26@gmail.com>
2019-01-05 15:57:09 +0530

    implements display tasks

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { TaskManagementComponent } from './task-management/task-management.component';
+import { AddtasksComponent } from './task-management/addtasks/addtasks.component';
+import { LoginSignupComponent } from './user-management/login-signup/login-signup.component';
+const routes: Routes = [
+  {path : "" ,pathMatch:"full",component:LoginSignupComponent},
+  {path: 'displaytask' ,pathMatch:"full" ,component: TaskManagementComponent},
+  {path: 'addtasks' ,pathMatch:"full", component:AddtasksComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const rountingComponent = [TaskManagementComponent,AddtasksComponent]
